e5c9a312a1bac92e7174a8ec03092560b62eed79
Vyshnovetskyi Volodymyr <vyshnovetskyi.volodymyr@gmail.com>
2026-07-14 13:27:27 +0200

    cewcdc

--- SmartTable.Client/Services/CourseDateService.cs
+++ SmartTable.Client/Services/CourseDateService.cs
@@ -1,57 +1,38 @@
-@using SmartTable.Client.Models
-@inject SmartTable.Client.Services.LocalizationService LocalizationSvc
+using System.Net.Http.Json;
+using SmartTable.Client.Models;
 
-<MudDialog>
-    <DialogContent>
-        <MudForm @ref="_form" @bind-IsValid="_isValid">
-            <MudStack Spacing="3">
-                <MudDatePicker @bind-Date="_startDate"
-                               Label="@LocalizationSvc.GetString("StartDateLabel")"
-                               Variant="Variant.Outlined"
-                               DateFormat="dd.MM.yyyy"
-                               Required="true"
-                               RequiredError="@LocalizationSvc.GetString("RequiredField")" />
+namespace SmartTable.Client.Services;
 
-                <MudDatePicker @bind-Date="_endDate"
-                               Label="@LocalizationSvc.GetString("EndDateLabelShort")"
-                               Variant="Variant.Outlined"
-                               DateFormat="dd.MM.yyyy" />
-            </MudStack>
-        </MudForm>
-    </DialogContent>
-    <DialogActions>
-        <MudButton OnClick="Cancel" Color="Color.Default" Variant="Variant.Text">
-            @LocalizationSvc.GetString("Cancel")
-        </MudButton>
-        <MudButton OnClick="Submit" Color="Color.Primary" Variant="Variant.Filled" Disabled="!_isValid || _startDate is null">
-            @LocalizationSvc.GetString("Add")
-        </MudButton>
-    </DialogActions>
-</MudDialog>
+public interface ICourseDateService
+{
+    Task<List<CourseDate>> GetAllAsync(Guid? courseId = null);
+    Task<CourseDate?> CreateAsync(CourseDate model);
+    Task<bool> DeleteAsync(Guid id);
+}
 
-@code {
-    [CascadingParameter] IMudDialogInstance MudDialog { get; set; } = null!;
-    [Parameter] public Guid CourseId { get; set; }
+public class CourseDateService : ICourseDateService
+{
+    private readonly HttpClient _http;
+    public CourseDateService(HttpClient http) => _http = http;
 
-    private MudForm _form = null!;
-    private bool _isValid;
-    private DateTime? _startDate = DateTime.Today;
-    private DateTime? _endDate;
-
-    private async Task Submit()
+    public async Task<List<CourseDate>> GetAllAsync(Guid? courseId = null)
     {
-        await _form.ValidateAsync();
-        if (!_isValid || _startDate is null) return;
-
-        var model = new CourseDate
-        {
-            CourseId = CourseId,
-            StartDate = _startDate.Value,
-            EndDate = _endDate
-        };
-
-        MudDialog.Close(DialogResult.Ok(model));
+        var url = courseId.HasValue ? $"api/coursedates?courseId={courseId}" : "api/coursedates";
+        var result = await _http.GetFromJsonAsync<List<CourseDate>>(url);
+        return result ?? new List<CourseDate>();
     }
 
-    private void Cancel() => MudDialog.Cancel();
+    public async Task<CourseDate?> CreateAsync(CourseDate model)
+    {
+        var response = await _http.PostAsJsonAsync("api/coursedates", model);
+        if (response.IsSuccessStatusCode)
+            return await response.Content.ReadFromJsonAsync<CourseDate>();
+        return null;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var response = await _http.DeleteAsync($"api/coursedates/{id}");
+        return response.IsSuccessStatusCode;
+    }
 }
